071fcc4c5511f52c1c36cc78faee66138c9fae8c
nodeJayS <shinjayw@gmail.com>
2020-12-11 18:28:06 -0500

    Initial Post Entity

--- backend/Fakebook/Fakebook.DataAccess/Model/PostEntity.cs
+++ backend/Fakebook/Fakebook.DataAccess/Model/PostEntity.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace Fakebook.DataAccess.Model
 {
-    class PostEntity
+    public class PostEntity
     {
+        public PostEntity()
+        {
+            Comments = new HashSet<CommentEntity>();
+            Likes = new HashSet<LikeEntity>();
+        }
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ParentId { get; set; }
+        string Content { get; set; }
+        DateTime CreatedAt { get; set; }
+        string Picture { get; set; }
+        public virtual ICollection<CommentEntity> Comments { get; set; }
+        public virtual ICollection<LikeEntity> Likes { get; set; }
     }
 }
