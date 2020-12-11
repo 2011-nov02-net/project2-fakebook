358f0e0e9ff1879a0dab0d6ab33a0ebe766c9e75
nodeJayS <shinjayw@gmail.com>
2020-12-11 18:31:50 -0500

    Like entity

---
+++ backend/Fakebook/Fakebook.DataAccess/Model/LikeEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fakebook.DataAccess.Model
+{
+    public class LikeEntity
+    {
+        public int PostId { get; set; }
+        public int UserId { get; set; }
+    }
+}
