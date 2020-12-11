2f7526eefc9481fe3f465161c904ce8b2eabec0d
Damien Bevins <damien.bevins@outlook.com>
2020-12-11 17:27:33 -0600

    Add UserEntity in the DataAccess class library

---
+++ backend/Fakebook/Fakebook.DataAccess/Model/UserEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fakebook.DataAccess.Model
+{
+    public class UserEntity
+    {
+        public UserEntity() {
+            Followers = new HashSet<UserEntity>();
+            Followees = new HashSet<UserEntity>();
+            Posts = new HashSet<PostEntity>();
+            PostsLiked = new HashSet<PostEntity>();
+        }
+
+        public int Id { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Status { get; set; }
+
+        public virtual ICollection<UserEntity> Followers { get; set; }
+        public virtual ICollection<UserEntity> Followees { get; set; }
+        public virtual ICollection<PostEntity> Posts { get; set; }
+        public virtual ICollection<PostEntity> PostsLiked { get; set; }
+    }
+}
