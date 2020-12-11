66f2ef873f320b49b9afd39f9e3d733e59ffec9e
Damien Bevins <damien.bevins@outlook.com>
2020-12-11 17:35:00 -0600

    Add FakebookContext and reference to the SqlServer NuGet package

---
+++ backend/Fakebook/Fakebook.DataAccess/Model/FakebookContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Fakebook.DataAccess.Model
+{
+    public class FakebookContext : DbContext
+    {
+        public FakebookContext([NotNull] DbContextOptions options) : 
+            base(options) {
+        }
+
+        public DbSet<UserEntity> UserEntities { get; set; }
+        public DbSet<PostEntity> PostEntities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
