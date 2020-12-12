816c23d5b9fb822101847e9384cbaafb2d2127a9
nodeJayS <shinjayw@gmail.com>
2020-12-12 11:07:48 -0500

    Removed redundant User property in Like Entity

--- backend/Fakebook/Fakebook.DataAccess/Model/LikeEntity.cs
+++ backend/Fakebook/Fakebook.DataAccess/Model/LikeEntity.cs
@@ -8,7 +8,6 @@ namespace Fakebook.DataAccess.Model
     {
         public int Id { get; set; }
         public int PostId { get; set; }
-        public PostEntity Post { get; set; }
         public int UserId { get; set; }
         public virtual PostEntity Post { get; set; }
         public virtual UserEntity User { get; set; }
