2431c4a58df2c1974244ae8d2c48e8cafc4853c8
Damien Bevins <damien.bevins@outlook.com>
2020-12-12 14:09:28 -0500

    Add migrations script and stage the migration changes before updating the database with them.

---
+++ backend/Fakebook/Fakebook.DataAccess/Migrations/20201212190122_InitialCreate.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fakebook.DataAccess.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Fakebook");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                schema: "Fakebook",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProfilePictureUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BirthDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Follow",
+                schema: "Fakebook",
+                columns: table => new
+                {
+                    FollowerId = table.Column<int>(type: "int", nullable: false),
+                    FolloweeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("Pk_FollowEntity", x => new { x.FollowerId, x.FolloweeId });
+                    table.ForeignKey(
+                        name: "FK_Follow_FolloweeId",
+                        column: x => x.FolloweeId,
+                        principalSchema: "Fakebook",
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Follow_FollowerId",
+                        column: x => x.FollowerId,
+                        principalSchema: "Fakebook",
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Post",
+                schema: "Fakebook",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    Content = table.Column<string>(type: "string", nullable: false),
+                    Picture = table.Column<string>(type: "string", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "(getdatetime())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Post_UserId",
+                        column: x => x.UserId,
+                        principalSchema: "Fakebook",
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comment",
+                schema: "Fakebook",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    PostId = table.Column<int>(type: "int", nullable: false),
+                    ParentId = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "smalldatetime", nullable: false, defaultValueSql: "(getdate())"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comment", x => x.Id);
+                    table.ForeignKey(
+                        name: "Fk_Comment_Comment",
+                        column: x => x.ParentId,
+                        principalSchema: "Fakebook",
+                        principalTable: "Comment",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "Fk_Comment_Post",
+                        column: x => x.PostId,
+                        principalSchema: "Fakebook",
+                        principalTable: "Post",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_COMMENT_USER",
+                        column: x => x.UserId,
+                        principalSchema: "Fakebook",
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Like",
+                schema: "Fakebook",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PostId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Like", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Like_Post",
+                        column: x => x.PostId,
+                        principalSchema: "Fakebook",
+                        principalTable: "Post",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Like_User",
+                        column: x => x.UserId,
+                        principalSchema: "Fakebook",
+                        principalTable: "User",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_ParentId",
+                schema: "Fakebook",
+                table: "Comment",
+                column: "ParentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_PostId",
+                schema: "Fakebook",
+                table: "Comment",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comment_UserId",
+                schema: "Fakebook",
+                table: "Comment",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Follow_FolloweeId",
+                schema: "Fakebook",
+                table: "Follow",
+                column: "FolloweeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Like_PostId",
+                schema: "Fakebook",
+                table: "Like",
+                column: "PostId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Like_UserId",
+                schema: "Fakebook",
+                table: "Like",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Post_UserId",
+                schema: "Fakebook",
+                table: "Post",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comment",
+                schema: "Fakebook");
+
+            migrationBuilder.DropTable(
+                name: "Follow",
+                schema: "Fakebook");
+
+            migrationBuilder.DropTable(
+                name: "Like",
+                schema: "Fakebook");
+
+            migrationBuilder.DropTable(
+                name: "Post",
+                schema: "Fakebook");
+
+            migrationBuilder.DropTable(
+                name: "User",
+                schema: "Fakebook");
+        }
+    }
+}
---
+++ backend/fakebook.sql
@@ -0,0 +1,101 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF SCHEMA_ID(N'Fakebook') IS NULL EXEC(N'CREATE SCHEMA [Fakebook];');
+GO
+
+CREATE TABLE [Fakebook].[User] (
+    [Id] int NOT NULL IDENTITY,
+    [ProfilePictureUrl] nvarchar(max) NULL,
+    [FirstName] nvarchar(max) NOT NULL,
+    [LastName] nvarchar(max) NOT NULL,
+    [Email] nvarchar(max) NOT NULL,
+    [PhoneNumber] nvarchar(max) NULL,
+    [BirthDate] datetime2 NOT NULL,
+    [Status] nvarchar(max) NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Fakebook].[Follow] (
+    [FollowerId] int NOT NULL,
+    [FolloweeId] int NOT NULL,
+    CONSTRAINT [Pk_FollowEntity] PRIMARY KEY ([FollowerId], [FolloweeId]),
+    CONSTRAINT [FK_Follow_FolloweeId] FOREIGN KEY ([FolloweeId]) REFERENCES [Fakebook].[User] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Follow_FollowerId] FOREIGN KEY ([FollowerId]) REFERENCES [Fakebook].[User] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [Fakebook].[Post] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] int NOT NULL,
+    [Content] string NOT NULL,
+    [Picture] string NOT NULL,
+    [CreatedAt] datetime2 NOT NULL DEFAULT ((getdatetime())),
+    CONSTRAINT [PK_Post] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Post_UserId] FOREIGN KEY ([UserId]) REFERENCES [Fakebook].[User] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [Fakebook].[Comment] (
+    [Id] int NOT NULL IDENTITY,
+    [UserId] int NOT NULL,
+    [PostId] int NOT NULL,
+    [ParentId] int NOT NULL,
+    [CreatedAt] smalldatetime NOT NULL DEFAULT ((getdate())),
+    [Content] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Comment] PRIMARY KEY ([Id]),
+    CONSTRAINT [Fk_Comment_Comment] FOREIGN KEY ([ParentId]) REFERENCES [Fakebook].[Comment] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [Fk_Comment_Post] FOREIGN KEY ([PostId]) REFERENCES [Fakebook].[Post] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_COMMENT_USER] FOREIGN KEY ([UserId]) REFERENCES [Fakebook].[User] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [Fakebook].[Like] (
+    [Id] int NOT NULL IDENTITY,
+    [PostId] int NOT NULL,
+    [UserId] int NOT NULL,
+    CONSTRAINT [PK_Like] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Like_Post] FOREIGN KEY ([PostId]) REFERENCES [Fakebook].[Post] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Like_User] FOREIGN KEY ([UserId]) REFERENCES [Fakebook].[User] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_Comment_ParentId] ON [Fakebook].[Comment] ([ParentId]);
+GO
+
+CREATE INDEX [IX_Comment_PostId] ON [Fakebook].[Comment] ([PostId]);
+GO
+
+CREATE INDEX [IX_Comment_UserId] ON [Fakebook].[Comment] ([UserId]);
+GO
+
+CREATE INDEX [IX_Follow_FolloweeId] ON [Fakebook].[Follow] ([FolloweeId]);
+GO
+
+CREATE INDEX [IX_Like_PostId] ON [Fakebook].[Like] ([PostId]);
+GO
+
+CREATE INDEX [IX_Like_UserId] ON [Fakebook].[Like] ([UserId]);
+GO
+
+CREATE INDEX [IX_Post_UserId] ON [Fakebook].[Post] ([UserId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20201212185536_InitialCreate', N'5.0.1');
+GO
+
+COMMIT;
+GO
+
