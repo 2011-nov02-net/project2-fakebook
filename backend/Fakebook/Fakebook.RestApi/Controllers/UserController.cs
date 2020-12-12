0628b9122f303b5f44ac1fd67abc9bd7c743b2f4
Jordan <jordaneddygarcia@gmail.com>
2020-12-12 15:07:46 -0600

    g Modified User Repo to have an interface

---
+++ backend/Fakebook/Fakebook.RestApi/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Fakebook.DataAccess.Model.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fakebook.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    // /api/user
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepo _repo;
+
+        public UserController(IUserRepo repository)
+        {
+            _repo = repository;
+        }
+
+        // GET: api/user
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var users = _repo.GetAllUsers();
+            return Ok(users);
+
+        }
+    }
+}
