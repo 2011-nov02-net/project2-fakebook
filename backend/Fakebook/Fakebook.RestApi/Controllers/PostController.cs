c9edf87ed40342b27ad9625d4855429e0748c392
nodeJayS <shinjayw@gmail.com>
2020-12-13 13:54:12 -0500

    Newsfeed Get method

--- backend/Fakebook/Fakebook.RestApi/Controllers/PostController.cs
+++ backend/Fakebook/Fakebook.RestApi/Controllers/PostController.cs
@@ -2,6 +2,8 @@
 using Fakebook.Domain.Repository;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Fakebook.RestApi.Controllers
 {
@@ -27,10 +29,39 @@ namespace Fakebook.RestApi.Controllers
         }
         // Gets all posts by a specific user id
         [HttpGet("{id}")]
-        public async Task<IActionResult> Get(int id)
+        public async Task<IActionResult> GetUserPosts(int id)
         {
             var posts = await _postRepo.GetPostsByUserId(id);
             return Ok(posts);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetNewsfeedPosts(int id)
+        {
+            var currentUser = await _userRepo.GetUserById(id);
+            var result = new List<Post>();
+            foreach(var followee in currentUser.Followees) // Iterate through list of people user follows
+            {
+                if (followee.Posts != null) // Check if the followee has made any posts
+                {
+                    var followeePosts = followee.Posts;
+                    if (followeePosts.Count < 3) // If followee has less than 3 posts, add all posts to result
+                    {
+                        foreach (var post in followeePosts)
+                        {
+                            result.Add(post);
+                        }
+                    }
+                    else
+                    {
+                        for (var i = 0; i < 3; i++) // Add up to 3 posts from the followee to the result
+                        {
+                            result.Add(followeePosts.ElementAt(i));
+                        }
+                    }
+                }
+            }
+            result.OrderBy(p => p.CreatedAt); // Order list by the date the posts were created
+            return Ok(result);
         }
     }
 }
