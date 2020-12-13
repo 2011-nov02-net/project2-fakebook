4da5517bbdec4e069a23b2d19c48fe9aedf8d9f0
Damien Bevins <damien.bevins@outlook.com>
2020-12-13 11:01:16 -0600

    Staged migrations for the database.

---
+++ backend/Fakebook/Fakebook.DataAccess/Migrations/20201213170020_LikeCompositeKey.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fakebook.DataAccess.Migrations
+{
+    public partial class LikeCompositeKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Like",
+                schema: "Fakebook",
+                table: "Like");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Like_UserId",
+                schema: "Fakebook",
+                table: "Like");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                schema: "Fakebook",
+                table: "Like");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "Pk_LikeEntity",
+                schema: "Fakebook",
+                table: "Like",
+                columns: new[] { "UserId", "PostId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "Pk_LikeEntity",
+                schema: "Fakebook",
+                table: "Like");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                schema: "Fakebook",
+                table: "Like",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Like",
+                schema: "Fakebook",
+                table: "Like",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Like_UserId",
+                schema: "Fakebook",
+                table: "Like",
+                column: "UserId");
+        }
+    }
+}
