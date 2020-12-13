ecf1ac44944d35ba09c58d8355984daca399a959
nodeJayS <shinjayw@gmail.com>
2020-12-13 13:26:33 -0500

    Initial Post Controller

---
+++ backend/Fakebook/Fakebook.RestApi/Controllers/PostController.cs
@@ -0,0 +1,36 @@
+﻿using Fakebook.Domain;
+using Fakebook.Domain.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Fakebook.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+
+        private readonly IPostRepo _postRepo;
+        private readonly IUserRepo _userRepo;
+
+        public PostController(IPostRepo postRepo, IUserRepo userRepo)
+        {
+            _postRepo = postRepo;
+            _userRepo = userRepo;
+        }
+        // Gets all posts. Use this for newsfeed
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var posts = await _postRepo.GetAllPosts();
+            return Ok(posts);
+        }
+        // Gets all posts by a specific user id
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var posts = await _postRepo.GetPostsByUserId(id);
+            return Ok(posts);
+        }
+    }
+}
