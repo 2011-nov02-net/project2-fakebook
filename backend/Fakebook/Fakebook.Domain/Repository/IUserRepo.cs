25edee9dbe9ee0b9f124cf43f7b6f56ea643a2c3
nodeJayS <shinjayw@gmail.com>
2020-12-13 11:25:41 -0500

    Moved repository to Domain folder

---
+++ backend/Fakebook/Fakebook.Domain/Repository/IUserRepo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Fakebook.Domain.Repository
+{
+    public interface IUserRepo
+    {
+        List<User> GetAllUsers();
+        Task<List<User>> GetAllUsersAsync();
+        Task<User> GetUserByEmail(string email);
+        Task<User> GetUserById(int id);
+    }
+}
