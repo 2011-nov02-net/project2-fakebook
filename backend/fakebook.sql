5b6b1996b227652c147e7ea3f6bfd43a45b16f37
Damien Bevins <damien.bevins@outlook.com>
2020-12-15 17:09:18 -0600

    Alter the FakebookContext to allow for a nullable profile picture string, and the migrations were created.

--- backend/fakebook.sql
+++ backend/fakebook.sql
@@ -127,3 +127,22 @@ GO
 COMMIT;
 GO
 
+BEGIN TRANSACTION;
+GO
+
+DECLARE @var1 sysname;
+SELECT @var1 = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Fakebook].[Post]') AND [c].[name] = N'Picture');
+IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [Fakebook].[Post] DROP CONSTRAINT [' + @var1 + '];');
+ALTER TABLE [Fakebook].[Post] ALTER COLUMN [Picture] nvarchar(max) NULL;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20201215230810_PictureStringNullable', N'5.0.1');
+GO
+
+COMMIT;
+GO
+
