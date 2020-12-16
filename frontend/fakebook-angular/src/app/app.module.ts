84f915506194e26c7aa736bf78a2fe7b2771d041
Damien Bevins <damien.bevins@outlook.com>
2020-12-16 15:14:03 -0600

    Alterd the app.module.ts to include the components that were generated.

--- frontend/fakebook-angular/src/app/app.module.ts
+++ frontend/fakebook-angular/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsfeedComponent } from './component/newsfeed/newsfeed.component';
+import { PostViewComponent } from './component/post-view/post-view.component';
+import { UserViewComponent } from './component/user-view/user-view.component';
+import { UserProfileViewComponent } from './component/user-profile-view/user-profile-view.component';
+import { ProfilePictureViewComponent } from './component/profile-picture-view/profile-picture-view.component';
+import { CommentViewComponent } from './component/comment-view/comment-view.component';
+import { LikeViewComponent } from './component/like-view/like-view.component';
+import { UserControlsComponent } from './component/user-controls/user-controls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsfeedComponent,
+    PostViewComponent,
+    UserViewComponent,
+    UserProfileViewComponent,
+    ProfilePictureViewComponent,
+    CommentViewComponent,
+    LikeViewComponent,
+    UserControlsComponent
   ],
   imports: [
     BrowserModule,
