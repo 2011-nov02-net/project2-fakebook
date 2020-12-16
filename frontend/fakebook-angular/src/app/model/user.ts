46cbf468ebefb0620aafb413684f1e2b2bcf4958
Damien Bevins <damien.bevins@outlook.com>
2020-12-16 15:01:21 -0600

    Stubbed out the model classes for the front end to use.

---
+++ frontend/fakebook-angular/src/app/model/user.ts
@@ -0,0 +1,13 @@
+export interface User {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string | undefined;
+  password: string;
+  profilePictureUrl: string | undefined;
+  status: string | undefined;
+  birthDate: Date;
+  followerIds: number[];
+  followeeIds: number[];
+}
