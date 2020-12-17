bf5a110cd1973aadad5ca27fa3590797a71bdf84
Damien Bevins <damien.bevins@outlook.com>
2020-12-17 14:26:37 -0600

    Add documentation on ICommentRepo and CommentRepo.

--- backend/Fakebook/Fakebook.Domain/Repository/CommentRepo.cs
+++ backend/Fakebook/Fakebook.Domain/Repository/CommentRepo.cs
@@ -11,13 +11,44 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Fakebook.Domain.Repository
 {
+    /// <summary>
+    /// Repository that manages comments within the application
+    /// </summary>
     public class CommentRepo : ICommentRepo
     {
         private readonly FakebookContext _context;
 
+        /// <summary>
+        /// Creates a new instance of a CommentRepo
+        /// </summary>
+        /// <param name="context">The context in which the CommentRepo interacts with</param>
         public CommentRepo(FakebookContext context) {
             _context = context;
         }
+
+        /// <summary>
+        /// Creates a new comment with the given comment data.
+        /// </summary>
+        /// <param name="comment">The object containing the comment data</param>
+        /// <returns>A boolean as to whether the creation succeeded or failed</returns>
+        public async Task<bool> CreateAsync(Comment comment) {
+            try {
+                comment.NullCheck(nameof(comment));
+
+                var entity = DbEntityConverter.ToCommentEntity(comment);
+                await _context.CommentEntities.AddAsync(entity);
+                await _context.SaveChangesAsync();
+            } catch {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Retrieves all of the Comments as an IEnumerable.
+        /// </summary>
+        /// <returns>A collection of all Comment objects</returns>
         public async Task<IEnumerable<Comment>> GetAllAsync() {
             var comments = await _context.CommentEntities
                 .Include(c => c.Post)
@@ -36,6 +67,11 @@ namespace Fakebook.Domain.Repository
                 .ToList();
         }
 
+        /// <summary>
+        /// Gets a Comment by its id.
+        /// </summary>
+        /// <param name="commentId">The id of the comment</param>
+        /// <returns>A 'Comment' object, if it is found, or else it returns null</returns>
         public async Task<Comment> GetCommentByIdAsync(int userId) {
             var comments = await _context.CommentEntities
                 .Include(c => c.Post)
@@ -54,6 +90,11 @@ namespace Fakebook.Domain.Repository
             return DbEntityConverter.ToComment(item);
         }
 
+        /// <summary>
+        /// Gets a collection of 'Comment' object by a collection of ids
+        /// </summary>
+        /// <param name="ids">A set of comment ids</param>
+        /// <returns>A collection of comments that have any of the ids passed in</returns>
         public async Task<IEnumerable<Comment>> GetCommentsByIdsAsync(ICollection<int> ids) {
             var comments = await _context.CommentEntities
                 .Include(c => c.Post)
@@ -72,6 +113,11 @@ namespace Fakebook.Domain.Repository
                 .Select(c => DbEntityConverter.ToComment(c));
         }
 
+        /// <summary>        
+        /// /// Gets the comments associated with a user's id
+        /// </summary>
+        /// <param name="userId">The id of the user that posted the comments</param>
+        /// <returns>A collection of 'Comment' objects by a user's id</returns>
         public async Task<IEnumerable<Comment>> GetCommentsByUserIdAsync(int userId) {
             var comments = _context.CommentEntities
                 .Include(c => c.Post)
@@ -89,6 +135,11 @@ namespace Fakebook.Domain.Repository
                 .Select(c => DbEntityConverter.ToComment(c));
         }
 
+        /// <summary>
+        /// Get the comments associated with a post
+        /// </summary>
+        /// <param name="postId">The id of the post</param>
+        /// <returns>A collection of 'Comment' objects that are associated with</returns>
         public async Task<IEnumerable<Comment>> GetCommentsByPostIdAsync(int postId) {
             var comments = _context.CommentEntities
                 .Include(c => c.Post)
@@ -106,20 +157,12 @@ namespace Fakebook.Domain.Repository
                 .Select(c => DbEntityConverter.ToComment(c));
         }
 
-        public async Task<bool> CreateAsync(Comment comment) {
-            try {
-                comment.NullCheck(nameof(comment));
-
-                var entity = DbEntityConverter.ToCommentEntity(comment);
-                await _context.CommentEntities.AddAsync(entity);
-                await _context.SaveChangesAsync();
-            } catch {
-                return false;
-            }
-
-            return true;
-        }
-
+        /// <summary>
+        /// Updates a comment with the given data.
+        /// If it is not found, the method returns false.
+        /// </summary>
+        /// <param name="comment">The data of the 'Comment' object to update</param>
+        /// <returns>True if the 'Comment' object was found and updated, false if it was not found or an issue occurred</returns>
         public async Task<bool> UpdateAsync(Comment comment) {
             try {
                 comment.NullCheck(nameof(comment));
@@ -146,6 +189,11 @@ namespace Fakebook.Domain.Repository
             return true;
         }
 
+        /// <summary>
+        /// Deletes a 'Comment' object by its id.
+        /// </summary>
+        /// <param name="id">The id of the 'Comment' object</param>
+        /// <returns>True if the object was found and deleted, false if it wasn't found or an exception occurred</returns>
         public async Task<bool> DeleteAsync(int id) {
             var entity = await _context.CommentEntities.FindAsync(id);
 
--- backend/Fakebook/Fakebook.Domain/Repository/ICommentRepo.cs
+++ backend/Fakebook/Fakebook.Domain/Repository/ICommentRepo.cs
@@ -3,15 +3,67 @@ using System.Threading.Tasks;
 
 namespace Fakebook.Domain.Repository
 {
+    /// <summary>
+    /// An interface that exposes a contract to perform actions on a given set of 
+    /// a collection of 'Comment' objects.
+    /// </summary>
     public interface ICommentRepo
     {
+        /// <summary>
+        /// Creates a new comment with the given comment data.
+        /// </summary>
+        /// <param name="comment">The object containing the comment data</param>
+        /// <returns>A boolean as to whether the creation succeeded or failed</returns>
         Task<bool> CreateAsync(Comment comment);
-        Task<bool> DeleteAsync(int id);
+
+        /// <summary>
+        /// Retrieves all of the Comments as an IEnumerable.
+        /// </summary>
+        /// <returns>A collection of all Comment objects</returns>
         Task<IEnumerable<Comment>> GetAllAsync();
-        Task<Comment> GetCommentByIdAsync(int userId);
+
+        /// <summary>
+        /// Gets a Comment by its id.
+        /// </summary>
+        /// <param name="commentId">The id of the comment</param>
+        /// <returns>A 'Comment' object, if it is found, or else it returns null</returns>
+        Task<Comment> GetCommentByIdAsync(int commentId);
+
+        /// <summary>
+        /// Gets a collection of 'Comment' object by a collection of ids
+        /// </summary>
+        /// <param name="ids">A set of comment ids</param>
+        /// <returns>A collection of comments that have any of the ids passed in</returns>
         Task<IEnumerable<Comment>> GetCommentsByIdsAsync(ICollection<int> ids);
+
+        /// <summary>        
+        /// /// Gets the comments associated with a user's id
+        /// </summary>
+        /// <param name="userId">The id of the user that posted the comments</param>
+        /// <returns>A collection of 'Comment' objects by a user's id</returns>
         Task<IEnumerable<Comment>> GetCommentsByUserIdAsync(int userId);
+
+        /// <summary>
+        /// Get the comments associated with a post
+        /// </summary>
+        /// <param name="postId">The id of the post</param>
+        /// <returns>A collection of 'Comment' objects that are associated with</returns>
         Task<IEnumerable<Comment>> GetCommentsByPostIdAsync(int postId);
+
+        /// <summary>
+        /// Updates a comment with the given data.
+        /// If it is not found, the method returns false.
+        /// </summary>
+        /// <param name="comment">The data of the 'Comment' object to update</param>
+        /// <returns>True if the 'Comment' object was found and updated, false if it was not found or an issue occurred</returns>
         Task<bool> UpdateAsync(Comment comment);
+
+        /// <summary>
+        /// Deletes a 'Comment' object by its id.
+        /// </summary>
+        /// <param name="id">The id of the 'Comment' object</param>
+        /// <returns>True if the object was found and deleted, false if it wasn't found or an exception occurred</returns>
+        Task<bool> DeleteAsync(int id);
+
     }
 }
