2bced1c709bee726824931bdeb5dddb2f9f1e248
Jordan <jordaneddygarcia@gmail.com>
2020-12-16 21:55:33 -0600

    Update UserController.cs
    Return an object rather than an OK(object)

--- backend/Fakebook/Fakebook.RestApi/Controllers/UserController.cs
+++ backend/Fakebook/Fakebook.RestApi/Controllers/UserController.cs
@@ -28,25 +28,29 @@ namespace Fakebook.RestApi.Controllers
         /// <returns></returns>
         // GET: api/user
         [HttpGet]
-        public async Task<IActionResult> Get()
+        public async Task<ActionResult<IEnumerable<User>>> Get()
         {
-            var users = await _userRepo.GetAllUsersAsync();
+            IEnumerable<User> users = await _userRepo.GetAllUsersAsync();
             return Ok(users);
         }
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> Get(int id)
+        public async Task<ActionResult<User>> Get(int id)
         {
-            var user = await _userRepo.GetUserByIdAsync(id);
-            return Ok(user);
+            User user = await _userRepo.GetUserByIdAsync(id);
+            if(user==null)
+            {
+                return NotFound();
+            }
+            return user;
         }
 
         // Gets all posts by a specific user id
         [HttpGet("{id}/Posts")]
-        public async Task<IActionResult> GetUserPosts(int id)
+        public async Task<ActionResult<List<Post>>> GetUserPosts(int id)
         {
-            var posts = await _postRepo.GetPostsByUserIdAsync(id);
-            return Ok(posts);
+            List<Post> posts = await _postRepo.GetPostsByUserIdAsync(id);
+            return posts;
         }
 
         [HttpPost]
