b063e99d7e6d11d30942a61ff9c4c38627ecfbd6
Damien Bevins <damien.bevins@outlook.com>
2020-12-17 14:06:48 -0600

    Added documentation comments on the extension methods that were created.

--- backend/Fakebook/Fakebook.Domain/Extension/ObjectExtensions.cs
+++ backend/Fakebook/Fakebook.Domain/Extension/ObjectExtensions.cs
@@ -2,8 +2,17 @@
 
 namespace Fakebook.Domain.Extension
 {
+    /// <summary>
+    /// Extensions made to be callable on the base .NET/C# object
+    /// </summary>
     public static class ObjectExtensions
     {
+        /// <summary>
+        /// Method that checks if a given object is null. 
+        /// If it is, an 'ArgumentException' will be thrown, to be caught later.
+        /// </summary>
+        /// <param name="target">The object that the method is being called on</param>
+        /// <param name="targetName">The name of the object that will be shown in the error message</param>
         public static void NullCheck(this object target, string targetName) {
             if(target is null) {
                 throw new ArgumentException($"{targetName} was null.");
--- backend/Fakebook/Fakebook.Domain/Extension/StringExtensions.cs
+++ backend/Fakebook/Fakebook.Domain/Extension/StringExtensions.cs
@@ -2,8 +2,17 @@
 
 namespace Fakebook.Domain.Extension
 {
+    /// <summary>
+    /// Static class containing extension methods to be called on strings
+    /// </summary>
     public static class StringExtensions
     {
+        /// <summary>
+        /// Checks if the string in which this is called on is null or empty.
+        /// If it is, an 'ArgumentException' will be thrown
+        /// </summary>
+        /// <param name="target">The string that the method is being called on</param>
+        /// <param name="targetName">The name of the variable that will be shown in the exception</param>
         public static void NullOrEmptyCheck(this string target, string targetName) {
             if(string.IsNullOrEmpty(target)) {
                 throw new ArgumentException($"{targetName} was null.");
