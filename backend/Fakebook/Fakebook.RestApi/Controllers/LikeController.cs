dc3b8e24c590503ff70c34d17c74e901466f3c51
Damien Bevins <damien.bevins@outlook.com>
2020-12-18 09:19:09 -0600

    Removed unused using statements inside LikeController.cs.

--- backend/Fakebook/Fakebook.RestApi/Controllers/LikeController.cs
+++ backend/Fakebook/Fakebook.RestApi/Controllers/LikeController.cs
@@ -1,10 +1,6 @@
 ﻿using Fakebook.Domain.Repository;
 
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace Fakebook.RestApi.Controllers
