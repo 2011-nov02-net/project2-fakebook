6266fb0ea070d58009208e72d113e95ea4ff3b49
Jordan <jordaneddygarcia@gmail.com>
2020-12-17 23:05:03 -0600

    [<feat>] If applied this commit will allow us to view the user page * utilized profile picture * added view for what a disabled button will look like * added template banner * added routing (check /user/1)
    <---------  TO DO ------------->
    * Add Create Post
    * Fix Friends
    * Add Friend's picture to followings
    * Clean up newsfeed for profile
    * Add Authentication

--- frontend/fakebook-angular/src/app/component/user-profile-view/user-profile-view.component.ts
+++ frontend/fakebook-angular/src/app/component/user-profile-view/user-profile-view.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router'; // getting the id number
+import { Post } from 'src/app/model/post';
+import { User } from 'src/app/model/user';
+import {  UserService } from '../../service/user.service';
 @Component({
   selector: 'app-user-profile-view',
+  providers: [UserService],
   templateUrl: './user-profile-view.component.html',
   styleUrls: ['./user-profile-view.component.css']
 })
 export class UserProfileViewComponent implements OnInit {
+  user : User | undefined; // our profile
+  posts : Post[] | undefined; // our users profile
 
-  constructor() { }
+  constructor(private httpService : UserService, 
+    private route: ActivatedRoute, // getting the id # in route
+    
+    ) { }
 
   ngOnInit(): void {
+    this.getUser();
   }
+  getUser(): void {
+    let tempId = ""; //the only way i could declare a variable that may accept a null value in teh future
+    
+    // get the id number from the route
+    if(this.route.snapshot.paramMap.get('id')!=null)  {
+      tempId += this.route.snapshot.paramMap.get('id')
+    }
+    // assign this to an id
+    const id = tempId;
 
+    this.httpService.getUser(id)
+      .subscribe(user => this.user = user);
+
+    this.httpService.getPosts(id)
+      .subscribe(posts => this.posts = posts)
+  }
 }
