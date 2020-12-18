7106a12ec3bba2eb737349f0a8af1ae384c0336e
Damien Bevins <damien.bevins@outlook.com>
2020-12-18 09:32:35 -0600

    Relocated the CommentTestData and stubbed out the 'UserTestData' and 'PostTestData' static classes.

---
+++ backend/Fakebook/Fakebook.UnitTests/TestData/UserTestData.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Fakebook.Domain;
+
+namespace Fakebook.UnitTests.TestData
+{
+    public static class UserTestData
+    {
+        public static class Create
+        {
+            public static IEnumerable<object[]> Valid
+            {
+                get
+                {
+                    return new List<object[]>
+                    {
+                        new object[] { new User { }, new Post { }, new Comment { },  }
+                    };
+                }
+            }
+
+            public static IEnumerable<object[]> Invalid
+            {
+                get
+                {
+                    return new List<object[]>
+                    {
+                        new object[] { new object(), new object() }
+                    };
+                }
+            }
+        }
+
+        public static class Read
+        {
+            public static IEnumerable<object[]> Valid
+            {
+                get
+                {
+                    return new List<object[]>
+                    {
+                        new object[] { new object(), new object() }
+                    };
+                }
+            }
+
+            public static IEnumerable<object[]> Invalid
+            {
+                get
+                {
+                    return new List<object[]>
+                    {
+                        new object[] { new object(), new object() }
+                    };
+                }
+            }
+        }
+
+        public static class Update
+        {
+            public static IEnumerable<object[]> Valid
+            {
+                get
+                {
+                    return new List<object[]>
+                    {
+                        new object[] { new object(), new object() }
+                    };
+                }
+            }
+
+            public static IEnumerable<object[]> Invalid
+            {
+                get
+                {
+                    return new List<object[]>
+                    {
+                        new object[] { new object(), new object() }
+                    };
+                }
+            }
+        }
+
+        public static class Delete
+        {
+            public static IEnumerable<object[]> Valid
+            {
+                get
+                {
+                    return new List<object[]>
+                    {
+                        new object[] { new object(), new object() }
+                    };
+                }
+            }
+
+            public static IEnumerable<object[]> Invalid
+            {
+                get
+                {
+                    return new List<object[]>
+                    {
+                        new object[] { new object(), new object() }
+                    };
+                }
+            }
+        }
+    }
+}
