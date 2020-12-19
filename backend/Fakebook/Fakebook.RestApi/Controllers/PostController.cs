f8544c0fd2efd7c616fded4b84d0acbb5b8995ec
Damien Bevins <damien.bevins@outlook.com>
2020-12-19 15:37:41 -0600

    Fixed issues with PostController.cs (a '}' wash misplaced).

--- backend/Fakebook/Fakebook.RestApi/Controllers/PostController.cs
+++ backend/Fakebook/Fakebook.RestApi/Controllers/PostController.cs
@@ -17,87 +17,72 @@ namespace Fakebook.RestApi.Controllers
         private readonly IUserRepo _userRepo;
         private readonly ICommentRepo _commentRepo;
 
-        public PostController(IPostRepo postRepo, IUserRepo userRepo, ICommentRepo commentRepo)
-        {
+        public PostController(IPostRepo postRepo, IUserRepo userRepo, ICommentRepo commentRepo) {
             _postRepo = postRepo;
             _userRepo = userRepo;
             _commentRepo = commentRepo;
         }
-        
+
         [HttpGet]
-        public async Task<IActionResult> Get()
-        {
+        public async Task<IActionResult> Get() {
             var posts = await _postRepo.GetAllPostsAsync();
             return Ok(posts);
         }
 
         [HttpPost]
         [Authorize]
-        public async Task<IActionResult> Post(PostApiModel apiModel)
-        {
+        public async Task<IActionResult> Post(PostApiModel apiModel) {
             var user = await _userRepo.GetUserByIdAsync(apiModel.UserId);
             var email = User.FindFirst(ct => ct.Type.Contains("nameidentifier")).Value;
-            if (email == user.Email)
-            {
-              try {
-                  var post = ApiModelConverter.ToPost(_userRepo, _commentRepo, apiModel);
+            if (email == user.Email) {
+                try {
+                    var post = ApiModelConverter.ToPost(_userRepo, _commentRepo, apiModel);
 
-                int result = await _postRepo.CreatePostAsync(post);
+                    int result = await _postRepo.CreatePostAsync(post);
 
-                if (result != -1) {
-                    // return Created()
-                    return Ok();
-                } else {
-                    return BadRequest();
+                    if (result != -1) {
+                        // return Created()
+                        return Ok();
+                    } else {
+                        return BadRequest();
+                    }
+                } catch (ArgumentException ex) {
+                    return BadRequest(ex.Message);
                 }
-            } catch(ArgumentException ex) {
-                return BadRequest(ex.Message);
-            }
-            else
-            {
+            } else {
                 return BadRequest();
             }
         }
 
         [HttpPut("{id}")]
         [Authorize]
-        public async Task<IActionResult> Put(PostApiModel apiModel)
-        {
+        public async Task<IActionResult> Put(PostApiModel apiModel) {
             var user = await _userRepo.GetUserByIdAsync(apiModel.UserId);
             var email = User.FindFirst(ct => ct.Type.Contains("nameidentifier")).Value;
-            if(email == user.Email)
-            {
+            if (email == user.Email) {
                 try {
                     var post = ApiModelConverter.ToPost(_userRepo, _commentRepo, apiModel);
                     await _postRepo.UpdatePostAsync(post);
                     return Ok();
-                } 
-                catch(ArgumentException ex) {
+                } catch (ArgumentException ex) {
                     return BadRequest(ex.Message);
-                } 
-                catch {
+                } catch {
                     return BadRequest();
                 }
-            }
-            else
-            {
+            } else {
                 return BadRequest();
             }
         }
 
         [HttpDelete("{id}")]
         [Authorize]
-        public async Task<IActionResult> Delete(int id)
-        {
+        public async Task<IActionResult> Delete(int id) {
             var post = await _postRepo.GetPostByIdAsync(id);
             var email = User.FindFirst(ct => ct.Type.Contains("nameidentifier")).Value;
-            if (email == post.User.Email)
-            {
+            if (email == post.User.Email) {
                 await _postRepo.DeletePostAsync(id);
                 return Ok();
-            }
-            else
-            {
+            } else {
                 return BadRequest();
             }
         }
