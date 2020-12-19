33b29e748cf580a64aaa0ae3b479c9d3ee9a1b02
Damien Bevins <damien.bevins@outlook.com>
2020-12-19 15:21:06 -0600

    Updated the Email regex to actually work.

--- backend/Fakebook/Fakebook.Domain/Utility/RegularExpressions.cs
+++ backend/Fakebook/Fakebook.Domain/Utility/RegularExpressions.cs
@@ -6,7 +6,7 @@
         public const string NoSpecialCharacters = @"^[A-Za-z0-9 '-,.#]*$";
         public const string NameCharacters = @"^[A-Za-z0-9 -]*$";
         public const string PasswordCharacters = @"^[A-Za-z0-9!@#$%&-_]{8,}$";
-        public const string EmailCharacters = @"^[a-zA-Z]+[a-zA-Z0-9\.]*\@[a-zA-Z]+[a-zA-Z0-9]+\.([a-zA-Z]+[a-zA-Z0-9]+){0,253}";
+        public const string EmailCharacters = @"^[a-zA-Z]+[a-zA-Z0-9\.]*[a-zA-Z0-9]*\@[a-zA-Z]+[a-zA-Z0-9]*\.([a-zA-Z]+){0,253}$";
         public const string PhoneNumberCharacters = @"^(\([0-9]{3}\) |[0-9]{3}[ -]?)?[0-9]{3}[ -]?[0-9]{4}$";
     }
 }
