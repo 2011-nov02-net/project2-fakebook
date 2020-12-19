65315ec0db47e2b3fe12883026785298a5c24dde
nodeJayS <shinjayw@gmail.com>
2020-12-19 14:17:23 -0500

    newsfeed requires authorization now

--- frontend/fakebook-angular/src/app/component/navbar-view/navbar-view.component.ts
+++ frontend/fakebook-angular/src/app/component/navbar-view/navbar-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OktaAuthService } from '@okta/okta-angular';
 
 @Component({
   selector: 'app-navbar-view',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar-view.component.css']
 })
 export class NavbarViewComponent implements OnInit {
+  isAuthenticated: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(public oktaAuth: OktaAuthService) { 
+    this.oktaAuth.$authenticationState.subscribe((isAuthenticated) =>
+      this.updateAuthState(this.isAuthenticated)
+    );
   }
 
+  updateAuthState(isAuthenticated: boolean) {
+    this.isAuthenticated = isAuthenticated;
+    if (isAuthenticated) {
+      this.oktaAuth.getUser().then(console.log)
+    }
+  }
+
+  async ngOnInit() {
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+  }
+
+  login() {
+    this.oktaAuth.signInWithRedirect();
+  }
 }
