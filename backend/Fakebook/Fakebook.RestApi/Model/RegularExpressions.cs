a0e9ab649fcec4ffd17c243300c83e611d587683
nodeJayS <60407405+nodeJayS@users.noreply.github.com>
2020-12-19 12:54:39 -0500

    Update RegularExpressions.cs

--- backend/Fakebook/Fakebook.RestApi/Model/RegularExpressions.cs
+++ backend/Fakebook/Fakebook.RestApi/Model/RegularExpressions.cs
@@ -10,7 +10,6 @@ namespace Fakebook.RestApi.Model
         public const string NumbersOnly = @"^[0-9]*$";
         public const string NoSpecialCharacters = @"^[A-Za-z0-9 '-,.#]*$";
         public const string NameCharacters = @"^[A-Za-z0-9 -]*$";
-        public const string UserNameCharacters = @"^[A-Za-z0-9]*$";
         public const string PasswordCharacters = @"^[A-Za-z0-9!@#$%&-_]{8,}$";
         public const string EmailCharacters = @"^[a-zA-Z]+[a-zA-Z0-9\.]*\@[a-zA-Z]+[a-zA-Z0-9]+\.([a-zA-Z]+[a-zA-Z0-9]+){0,253}";
         public const string PhoneNumberCharacters = @"^(\+[0-9]{0,3})?[ -]?[0-9]{3}[ -]?[0-9]{3}[ -]?[0-9]{4}";
