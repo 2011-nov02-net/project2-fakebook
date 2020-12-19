cdec0a85e8b8a1146fb21e76ab44bec5573abeda
Damien Bevins <damien.bevins@outlook.com>
2020-12-19 13:05:16 -0600

    Added DateTime extensions for checking if the date is in the past.

---
+++ backend/Fakebook/Fakebook.Domain/Extension/DateTimeExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fakebook.Domain.Extension
+{
+    public static class DateTimeExtensions
+    {
+        public static void EnforcePast(this DateTime datetime) {
+            if(!datetime.InPast()) {
+                throw new ArgumentException($"{nameof(datetime)} is not in the past");
+            }
+        }
+
+        public static bool InPast(this DateTime time) {
+            return time <= DateTime.Now;
+        }
+    }
+}
