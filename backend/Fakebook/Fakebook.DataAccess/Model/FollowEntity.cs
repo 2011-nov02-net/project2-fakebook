ed377979c5c6c113eeb9bc70340cc411c12a24a5
nodeJayS <shinjayw@gmail.com>
2020-12-19 22:51:19 -0500

    Created follow and unfollow repository methods and controllers

--- backend/Fakebook/Fakebook.DataAccess/Model/FollowEntity.cs
+++ backend/Fakebook/Fakebook.DataAccess/Model/FollowEntity.cs
@@ -4,6 +4,11 @@ namespace Fakebook.DataAccess.Model
 {
     public class FollowEntity
     {
+        public FollowEntity(int id, int userId)
+        {
+            FollowerId = id;
+            FolloweeId = userId;
+        }
         public int FollowerId { get; set; }
         public UserEntity Follower { get; set; }
         public int FolloweeId { get; set; }
--- backend/Fakebook/Fakebook.Domain/Repository/IPostRepo.cs
+++ backend/Fakebook/Fakebook.Domain/Repository/IPostRepo.cs
@@ -5,16 +5,17 @@ namespace Fakebook.Domain.Repository
 {
     public interface IPostRepo
     {
+        int CountLikes(int id);
+        Task<int> CountLikesAsync(int id);
         Task<bool> CreatePostAsync(Post post);
-        Task<Post> GetPostByIdAsync(int id);
+        Task<bool> DeletePostAsync(int id);
         Task<List<Post>> GetAllPostsAsync();
+        Task<List<Post>> GetFollowingPosts(int id);
+        Task<Post> GetPostByIdAsync(int id);
         Task<List<Post>> GetPostsByIdAsync(int id);
         Task<List<Post>> GetPostsByUserIdAsync(int id);
-        Task<int> CountLikesAsync(int id);
-        Task<bool> UpdatePostAsync(Post post);
         Task<bool> LikePostAsync(int id, int userId);
         Task<bool> UnlikePostAsync(int id, int userId);
-        Task<bool> DeletePostAsync(int id);
-        Task<List<Post>> GetFollowingPosts(int id);
+        Task<bool> UpdatePostAsync(Post post);
     }
 }
