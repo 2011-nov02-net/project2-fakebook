2c87db16b454269131a1abd444688af503d112b8
nodeJayS <shinjayw@gmail.com>
2020-12-19 23:23:43 -0500

    Follow service created

--- frontend/fakebook-angular/src/app/service/follow.service.ts
+++ frontend/fakebook-angular/src/app/service/follow.service.ts
@@ -1,17 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Post } from '../model/post';
 import { User } from '../model/user';
+import { Like } from '../model/like';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FollowService {
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  url = 'http://2011-project2-fakebook.azurewebsites.net/api/';
 
-  follow(follower: User, followee: User): boolean {
-    return false;
+  follow(follower: User, followee: User): any {
+    let httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.post(`${this.url}User/${follower}/follow/${followee}`, httpOptions)
+      .toPromise().then(res => console.log(JSON.stringify(res)));
   }
 
-  unfollow(follower: User, followee: User): boolean {
-    return false;
+  unfollow(follower: User, followee: User): any {
+    let httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.post(`${this.url}User/${follower}/unfollow/${followee}`, httpOptions)
+      .toPromise().then(res => console.log(JSON.stringify(res)));
   }
 }
