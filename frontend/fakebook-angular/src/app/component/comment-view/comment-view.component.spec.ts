2f77cad57112e3e23fa538e531bea505fda5c745
Damien Bevins <damien.bevins@outlook.com>
2020-12-22 16:20:15 -0600

    Constructing a comment view now passes.

--- frontend/fakebook-angular/src/app/component/comment-view/comment-view.component.spec.ts
+++ frontend/fakebook-angular/src/app/component/comment-view/comment-view.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CommentService } from 'src/app/service/comment.service';
 
 import { CommentViewComponent } from './comment-view.component';
 
@@ -6,9 +7,16 @@ describe('CommentViewComponent', () => {
   let component: CommentViewComponent;
   let fixture: ComponentFixture<CommentViewComponent>;
 
+  let fauxCommentService = {
+    delete(comment: Comment): Promise<number> {
+      return Promise.resolve(-1);
+    }
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CommentViewComponent ]
+      declarations: [ CommentViewComponent ],
+      providers: [{ provide: CommentService, useValue: fauxCommentService}]
     })
     .compileComponents();
   });
