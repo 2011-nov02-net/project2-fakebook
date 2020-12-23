9d4933be80a08a3ce47e32dfa5b576b65dc352ad
Damien Bevins <damien.bevins@outlook.com>
2020-12-22 19:41:50 -0600

    Progress thus far on the test for user-profile-view.

--- frontend/fakebook-angular/src/app/component/user-profile-view/user-profile-view.component.spec.ts
+++ frontend/fakebook-angular/src/app/component/user-profile-view/user-profile-view.component.spec.ts
@@ -1,4 +1,11 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OktaAuthService } from '@okta/okta-angular';
+import { NEVER, Observable, of } from 'rxjs';
+import { FollowService } from 'src/app/service/follow.service';
+import { PostService } from 'src/app/service/post.service';
+import { UserService } from 'src/app/service/user.service';
 
 import { UserProfileViewComponent } from './user-profile-view.component';
 
@@ -6,9 +13,39 @@ describe('UserProfileViewComponent', () => {
   let component: UserProfileViewComponent;
   let fixture: ComponentFixture<UserProfileViewComponent>;
 
+  let fauxOktaAuthService = {
+    getAccessToken(): string {
+      return '';
+    }
+  };
+
+  let fauxHttp = {
+    get(url: string, options: { headers: HttpHeaders }): Observable<any> {
+      return of(NEVER);
+    }
+  };
+
+  let fauxActivatedRoute = {
+    snapshot: {
+      paramMap: {
+        get(name: string): string | null {
+          return name;
+        }
+      }
+    }
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserProfileViewComponent ]
+      declarations: [ UserProfileViewComponent ],
+      providers: [
+        { provide: HttpClient, useValue: fauxHttp },
+        { provide: PostService, useValue: {} },
+        { provide: UserService, useValue: {} },
+        { provide: FollowService, useValue: {} },
+        { provide: ActivatedRoute, useValue: fauxActivatedRoute },
+        { provide: OktaAuthService, useValue: fauxOktaAuthService }
+      ]
     })
     .compileComponents();
   });
